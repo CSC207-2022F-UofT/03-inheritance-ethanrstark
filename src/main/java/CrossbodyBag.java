7c53da7606495ed64094b40db367539690eae461
ethanrstark <ethan.stark@mail.utoronto.ca>
2022-09-24 19:18:10 -0400

    Finished exercise.

--- src/main/java/CrossbodyBag.java
+++ src/main/java/CrossbodyBag.java
@@ -5,6 +5,45 @@
  * You may find the readings in chapter 3. Relationships between Classes
  * helpful while working through this exercise.
  */
+public class CrossbodyBag extends Bag {
+    public int numberOfStraps;
+    /**
+     * Creates a new CrossbodyBag with the given color,
+     * capacity, and number of straps
+     *
+     * @param color
+     * @param capacity
+     * @param numberOfStraps
+     */
+    public CrossbodyBag(String color, int capacity, int numberOfStraps) {
+        super(color, capacity);
+        this.numberOfStraps = numberOfStraps;
+    }
+
+    public int getNumberOfStraps() {
+        return this.numberOfStraps;
+    }
+
+    /**
+     * Increase the capacity of this bag by 2.
+     */
+    @Override
+    public void enhance() {
+        super.increaseCapacity(2);
+    }
+
+    /**
+     * Return the details of this Bag.
+     * @return
+     */
+    @Override
+    public String toString() {
+        return super.getColor() + " Crossbody Bag with " + this.numberOfStraps + " straps (" +
+                super.getNumberOfContents() + " / " + super.getCapacity() + ")";
+    }
+}
+
+
 
 /*
  * TODO: Create a public class named CrossbodyBag which is a subclass of Bag
